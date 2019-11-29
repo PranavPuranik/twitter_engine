02d9ac5911edde483eaa2c86b2c8818b864b7d13
subhambgh <subhama.sit@gmail.com>
2019-11-28 22:32:18 -0500

    deps commit

---
+++ deps/veritaserum/lib/veritaserum.ex
@@ -0,0 +1,118 @@
+defmodule Veritaserum do
+  @moduledoc """
+  Sentiment analisis based on AFINN-165, emojis and some enhancements.
+
+  Also supports:
+  - emojis (❤️, 😱...)
+  - boosters (*very*, *really*...)
+  - negators (*don't*, *not*...).
+  """
+
+  alias Veritaserum.Evaluator
+
+  @spec analyze(list(String.t()) | String.t()) :: list(integer) | integer
+  def analyze(input) when is_list(input) do
+    input
+    |> Stream.map(&analyze/1)
+    |> Enum.to_list()
+  end
+
+  @doc """
+  Returns a sentiment value for the given text
+
+      iex> Veritaserum.analyze(["I ❤️ Veritaserum", "Veritaserum is really awesome"])
+      [3, 5]
+
+      iex> Veritaserum.analyze("I love Veritaserum")
+      3
+  """
+  @spec analyze(String.t(), return: :score_and_marks) :: {number(), [{atom, number, String.t()}]}
+  def analyze(input, return: :score_and_marks) do
+    list_with_marks = get_list_with_marks(input)
+    score = get_score(list_with_marks)
+
+    {score, list_with_marks}
+  end
+
+  def analyze(input) do
+    {score, _} = analyze(input, return: :score_and_marks)
+
+    score
+  end
+
+  defp get_score(words) do
+    words
+    |> analyze_list
+    |> Enum.sum()
+  end
+
+  defp get_list_with_marks(input) do
+    input
+    |> clean
+    |> String.split()
+    |> mark_list
+    |> Enum.reverse()
+  end
+
+  defp mark_word(word) do
+    with {_, nil, _} <- {:negator, Evaluator.evaluate_negator(word), word},
+         {_, nil, _} <- {:booster, Evaluator.evaluate_booster(word), word},
+         {_, nil, _} <- {:emoticon, Evaluator.evaluate_emoticon(word), word},
+         {_, nil, _} <- {:word, Evaluator.evaluate_word(word), word},
+         do: {:neutral, 0, word}
+  end
+
+  defp mark_list([head | tail]) do
+    mark_list(tail, [mark_word(head)])
+  end
+
+  defp mark_list([head | tail], result) do
+    mark_list(tail, [mark_word(head) | result])
+  end
+
+  defp mark_list([], result), do: result
+
+  defp analyze_mark({type, score, _}) do
+    case type do
+      :word -> score
+      :emoticon -> score
+      _ -> 0
+    end
+  end
+
+  defp analyze_mark(mark, previous) do
+    case previous do
+      {:negator, _, _} ->
+        -analyze_mark(mark)
+
+      {:booster, booster_value, _} ->
+        analyze_mark(mark) |> apply_booster(booster_value)
+
+      _ ->
+        analyze_mark(mark)
+    end
+  end
+
+  defp analyze_list([head | tail]) do
+    analyze_list(tail, head, [analyze_mark(head)])
+  end
+
+  defp analyze_list([head | tail], previous, result) do
+    analyze_list(tail, head, [analyze_mark(head, previous) | result])
+  end
+
+  defp analyze_list([], _, result), do: result
+
+  defp apply_booster(word_value, booster) when word_value > 0, do: word_value + booster
+  defp apply_booster(word_value, booster) when word_value < 0, do: word_value - booster
+  defp apply_booster(word_value, _booster), do: word_value
+
+  defp clean(text) do
+    text
+    |> String.replace(~r/\n/, " ")
+    |> String.downcase()
+    |> String.replace(~r/[.,\/#!$%\^&\*;:{}=_`\"~()]/, " ")
+    #|> String.replace(Evaluator.emoticon_list(), "  ", insert_replaced: 1)
+    |> String.replace(~r/ {2,}/, " ")
+  end
+end
---
+++ deps/veritaserum/mix.exs
@@ -0,0 +1,72 @@
+defmodule Veritaserum.Mixfile do
+  use Mix.Project
+
+  @version "0.2.1"
+
+  def project do
+    [
+      app: :veritaserum,
+      version: @version,
+      elixir: "~> 1.3",
+      description: "Sentiment analysis based on afinn-165, emojis and some enhancements.",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: Coverex.Task],
+      aliases: aliases(),
+      package: package(),
+      deps: deps(),
+      docs: docs(),
+      source_url: github()
+    ]
+  end
+
+  def application do
+    [extra_applications: [:logger]]
+  end
+
+  defp package do
+    [
+      files: [
+        "config/facets/word.json",
+        "config/facets/negator.json",
+        "config/facets/booster.json",
+        "config/facets/emoticon.json",
+        "lib",
+        "mix.exs",
+        "README.md",
+        "LICENSE.md"
+      ],
+      links: %{"github" => github()},
+      maintainers: ["Unai Esteibar <uesteibar@gmail.com>"],
+      licenses: ["Apache 2.0"]
+    ]
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}",
+      main: "Veritaserum",
+      logo: "veritaserum_logo.png",
+      extras: ["README.md"]
+    ]
+  end
+
+  defp deps do
+    [
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:coverex, "~> 1.4", only: :test}
+    ]
+  end
+
+  defp aliases do
+    [
+      test: "test --cover"
+    ]
+  end
+
+  defp github do
+    "https://github.com/uesteibar/veritaserum"
+  end
+end
